6b15cca8cff04fe05fa841e1bddce776dbb6d418
Gaurav Kaushik <gk480555@gmail.com>
2025-10-29 00:59:01 +0530

    Create IMultiSigWallet interface
    Added interface for MultiSigWallet with transaction methods.

--- Contract/Solidity/interfaces/IMultiSigWallet.sol
+++ Contract/Solidity/interfaces/IMultiSigWallet.sol
@@ -1,1 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+/// @notice Interface for MultiSigWallet to decouple tests and external integrations.
+interface IMultiSigWallet {
+    function submitTransaction(address to, uint value, bytes calldata data) external returns (uint txId);
+    function confirmTransaction(uint txId) external;
+    function revokeConfirmation(uint txId) external;
+    function executeTransaction(uint txId) external;
+    function getOwners() external view returns (address[] memory);
+}
